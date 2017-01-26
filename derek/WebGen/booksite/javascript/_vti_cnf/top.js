08c2e9925b75c2f26ac5fb640055f689e4435e96
Derek Wilson <mail@derekwilson.net>
2017-01-26 23:01:07 +1300

    removed WebGen

--- derek/WebGen/booksite/javascript/_vti_cnf/top.js
+++
@@ -1,4 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|03 Mar 2009 17:49:30 -0000
-vti_extenderversion:SR|5.0.2.6790
-vti_backlinkinfo:VX|derek/WebGen/booksite/html/menu.htm
--- derek/WebGen/booksite/javascript/css.js
+++
@@ -1,8 +0,0 @@
-function GetOneCSSForBrowser(pathPrefix,cssfile)
-{
-	if (document.layers)
-		document.write("<link rel='stylesheet' type='text/css' href='" + pathPrefix + cssfile + "_ns.css'>");
-	else
-		document.write("<link rel='stylesheet' type='text/css' href='" + pathPrefix + cssfile + "_ie.css'>");
-}
-
